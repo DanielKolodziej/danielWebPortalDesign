7512e10ba4e05a46d28509c3c2a7f8bae55cd7ca
Daniel Kolodziej <dkolodz1@hawk.iit.edu>
2016-04-13 12:26:18 -0500

    added if/else statements to change css of elements

--- login.js
+++ login.js
@@ -2,7 +2,23 @@ function myFunction() {
     var emailVal = document.getElementById("userN").value;
 	var passVal = document.getElementById("password").value;
     // If the username is "testuser" and pass is "testpass"
-    
+    if (emailVal === "testuser" && passVal === "testpass") {
+        document.getElementById("userN").style.background="#00FF00";
+		document.getElementById("checkUser").checked = true; 
+		document.getElementById("password").style.background="#00FF00";
+		document.getElementById("checkPass").checked = true; 
+    // If the username is "testuser" and pass is wrong
+    } 
+    else if(emailVal === "testuser" && passVal !== "testpass")
+    {
+	document.getElementById("userN").style.background="#00FF00";
+	document.getElementById("checkUser").checked = true;
+	document.getElementById("password").style.background="#ff3333";
+    }
+    else {
+        document.getElementById("userN").style.background="#ff3333";
+	document.getElementById("password").style.background="#ff3333";
+    }
 }
 
 var alertButton = document.getElementById('log');
