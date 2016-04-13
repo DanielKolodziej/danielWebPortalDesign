3c7eff1899a7850c6d5204fdbaf7f0a8937df60b
Daniel Kolodziej <dkolodz1@hawk.iit.edu>
2016-04-13 12:22:25 -0500

    added function to show message upon clickk

--- login.js
+++ login.js
@@ -1,3 +1,12 @@
 var alertButton = document.getElementById('log');
 var textInput = document.getElementById('userN');
 var passInput = document.getElementById('password');
+alertButton.addEventListener('click', function(){
+	if (textInput.value === "testuser" && passInput.value === "testpass")
+	{
+		alert("You have successfully logged in, " + textInput.value + "!");
+	}
+	else{
+		alert("Error: credentials not found");
+	}
+});
